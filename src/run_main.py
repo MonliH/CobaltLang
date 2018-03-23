f2b131a5ca2018549605580d0c644a13240df268
Jonathan Li <jonathanliworld@gmail.com>
2018-03-23 16:28:03 -0400

    Fixed writing error

--- src/run_main.py
+++ src/run_main.py
@@ -9,7 +9,7 @@ def main(filename):
     file_contents = tokenizer.MakeTokens(filename)
     tokens = file_contents.get_tokens()
     writeToFile.BuildToCPP(tokens, filename).build()
-    os.system(f'cd {os.getcwd()} && go run {filename[0:-7] + ".go"}')
+    os.system(f"cd {os.getcwd()} && go run {filename[0:-7] + '.go'}")
 
 
 main(argv[1])
