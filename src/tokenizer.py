401daa335af5f6e314200d07eb65af91b330f3c2
Jonathan Li <jonathanliworld@gmail.com>
2018-03-23 20:28:36 -0400

    Added variable token detection

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -15,39 +15,61 @@ class MakeTokens:
     def get_tokens(self):
         word = ""
         string = ""
-        number = ""
         expr = ""
+        var_name = ""
         is_expr = False
         is_string = False
+        is_var = False
         for char in self.open_file():
             word += char
             if char == ";":
-                if expr != "" and is_expr:
+                if expr != "" and is_expr and not is_var:
                     self.tokens.append(["EXPR:{}".format(expr), self.line])
                     expr = ""
                 elif expr != "" and not is_expr:
                     self.tokens.append(["NUM:{}".format(expr), self.line])
                     expr = ""
+                if is_var and var_name != "":
+                    self.tokens.append([f"VAR:{var_name}", self.line])
+                    var_name = ""
                 self.line += 1
                 word = ""
+                is_expr = False
+                is_var = False
             elif word == "\n":
                 word = ""
             elif char == ":":
                 self.tokens.append(["COLON", self.line])
                 word = ""
+            elif char == "@":
+                is_var = True
+                word = ""
+            elif word == "var":
+                is_var = True
+                word = ""
+            elif char == "=":
+                var_name = var_name.replace(" ", "")
+                self.tokens.append([f"VAR:{var_name}", self.line])
+                self.tokens.append(["EQUALS", self.line])
+                var_name = ""
+                word = ""
+                is_var = False
+            elif is_var:
+                var_name += char
             elif word == "display":
                 self.tokens.append(["DISPLAY", self.line])
-            elif word.isdigit() or word == ".":
+                word = ""
+            elif word.isdigit() or char == ".":
                 expr += word
                 word = ""
-            elif word in "+/%*-":
+            elif char in "+/%*-":
                 expr += word
                 word = ""
                 is_expr = True
-            elif is_expr and word == ".":
+            elif is_expr and char == ".":
                 expr += word
                 word = ""
-            elif char == "\"":
+            elif char == "\"" and not is_var:
                 if not is_string:
                     is_string = True
                 elif is_string:
