4dfc7e35c4217cfb70101bf846018f6e3b0c36af
Jonathan Li <jonathanliworld@gmail.com>
2018-03-23 20:29:03 -0400

    Added variables

--- src/writeToFile.py
+++ src/writeToFile.py
@@ -17,12 +17,22 @@ class BuildToCPP:
             self.imported.append("fmt")
             self.imports.append("fmt")
 
-        if text[0:3] == "NUM":
+        if text[0:3] == "VAR":
+            self.go_code += f"fmt.Println({text[4:]})\n\t"
+        elif text[0:3] == "NUM":
             self.go_code += f"fmt.Println({text[4:]})\n\t"
         elif text[0:4] == "EXPR":
             self.go_code += f"fmt.Println({text[5:]})\n\t"
         elif text[0:6] == "STRING":
             self.go_code += f"fmt.Println({text[7:]})\n\t"
+
+    def do_var(self, value, varname):
+        if value[0:3] == "NUM":
+            self.go_code += f"{varname} := {value[4:]}\n\t"
+        elif value[0:4] == "EXPR":
+            self.go_code += f"{varname} := {value[5:]}\n\t"
+        elif value[0:6] == "STRING":
+            self.go_code += f"{varname} := {value[7:]}\n\t"
 
     def build(self):
         if not isfile(self.filename):
@@ -35,6 +45,9 @@ class BuildToCPP:
         while i < len(self.tokens):
             if f"{self.tokens[i][0]} {self.tokens[i + 1][0]}" == "DISPLAY COLON":
                 self.do_display(self.tokens[i + 2][0])
+                i += 3
+            elif f"{self.tokens[i][0][0:3]} {self.tokens[i + 1][0][0:6]}" == "VAR EQUALS":
+                self.do_var(self.tokens[i + 2][0], self.tokens[i][0][4:])
                 i += 3
 
         for i in range(len(self.imports)):
