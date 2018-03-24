f1b88806dcd65f8af2dd7c8bcbab3630738b2972
Jonathan Li <jonathanliworld@gmail.com>
2018-03-23 20:26:59 -0400

    Made exception handling

--- src/Cobalt.py
+++ src/Cobalt.py
@@ -1,1 +1,9 @@
-import run_main
+try:
+    import run_main
+except KeyboardInterrupt:
+    try:
+        from colorama import init, Fore
+        init()
+        print(Fore.RED + "You stopped the program!" + Fore.RESET)
+    except ImportError:
+        print("You stopped the program")
