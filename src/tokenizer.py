5a41395c42f1d8a371d599310130687544c5f55c
Jonathan Li <jonathanliworld@gmail.com>
2018-03-25 20:31:00 -0400

    Added partial if statement functionality

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -17,21 +17,30 @@ class MakeTokens:
         string = ""
         expr = ""
         var_name = ""
+        condition = ""
         is_expr = False
+        is_if = False
+        is_if_started = False
         is_string = False
         is_var = False
         for char in self.open_file():
             word += char
-            if char == ";":
+            if char == ";" or char == "{" and is_if or char == "}" and is_if_started:
                 if expr != "" and is_expr and not is_var:
-                    self.tokens.append(["EXPR:{}".format(expr), self.line])
+                    self.tokens.append([f"EXPR:{expr}", self.line])
                     expr = ""
                 elif expr != "" and not is_expr:
-                    self.tokens.append(["NUM:{}".format(expr), self.line])
+                    self.tokens.append([f"NUM:{expr}", self.line])
                     expr = ""
                 if is_var and var_name != "":
                     self.tokens.append([f"VAR:{var_name}", self.line])
                     var_name = ""
+                if is_if and condition != "":
+                    condition = condition.replace(" ", "")
+                    self.tokens.append([f"CONDITION:{condition}", self.line])
+                    is_if = False
+                    is_if_started = True
+
                 self.line += 1
                 word = ""
                 is_expr = False
@@ -44,6 +53,11 @@ class MakeTokens:
             elif char == "@":
                 is_var = True
                 word = ""
+            elif word == "if":
+                is_if = True
+                self.tokens.append("IF")
+            elif is_if:
+                condition += char
             elif word == "var":
                 is_var = True
                 word = ""
